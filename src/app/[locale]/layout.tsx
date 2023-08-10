7042efb8462834031b352ae22259a2d191b35d3b
Alexandre Cochet <alexandre.cochet0@gmail.com>
2023-08-10 15:35:12 +0200

    feat(i18n): i18n improvements

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { useLocale } from 'next-intl';
 import { notFound } from 'next/navigation';
-import '@/app/globals.css';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
