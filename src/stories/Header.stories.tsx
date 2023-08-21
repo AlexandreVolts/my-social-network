55553b99da2ba59161e021281f494dd0922b50ce
Alexandre Cochet <alexandre.cochet0@gmail.com>
2023-08-21 09:36:38 +0200

    feat(app): Header story layout updated

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -5,10 +5,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 const meta = {
   title: "Components/Header",
   component: Header,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
-    layout: "centered",
-  },
+  parameters: {},
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
