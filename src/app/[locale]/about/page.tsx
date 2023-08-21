5de63d8bdad70f401ec222271d83a8e17673343d
Alexandre Cochet <alexandre.cochet0@gmail.com>
2023-08-21 09:27:35 +0200

    feat(about): About page created

---
+++ src/app/[locale]/about/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Header } from "@/components/Header";
+
+export default function About() {
+  return (
+    <>
+      <Header />
+      <main className="flex flex-col items-center justify-between p-24">
+      </main>
+    </>
+  )
+}
