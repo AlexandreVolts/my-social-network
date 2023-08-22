5d1450ad358f2df2364f7b0dd3842cfcada0e4cb
Alexandre Cochet <alexandre.cochet0@gmail.com>
2023-08-22 11:25:56 +0200

    refactor(app): login and register pages updated with the useLogin hook

--- src/app/[locale]/register/page.tsx
+++ src/app/[locale]/register/page.tsx
@@ -2,20 +2,30 @@
 import { Footer } from "@/components/Footer";
 import { RegisterForm } from "@/components/RegisterForm";
 import { useLogin } from "@/hooks/useLogin";
+import { RegisterFormData } from "@/types/RegisterFormData";
 import { useRouter } from "next-intl/client";
 
 export default function Register() {
-  const handlers = useLogin();
+  const { values, handlers } = useLogin();
   const router = useRouter();
+
+  const onSubmit = (data: RegisterFormData) => {
+    handlers.register(
+      data,
+      () => {},
+      () => {}
+    );
+  };
 
   return (
     <>
       <main className="flex flex-col items-center justify-between p-24">
-          <h1 className="text-5xl p-8">My Social Network</h1>
-          <RegisterForm
-            onSubmit={handlers.register}
-            onSwitch={() => router.push("/login")}
-          />
+        <h1 className="text-5xl p-8">My Social Network</h1>
+        <RegisterForm
+          onSubmit={onSubmit}
+          onSwitch={() => router.push("/login")}
+          isLoading={values.isLoading}
+        />
       </main>
       <Footer />
     </>
