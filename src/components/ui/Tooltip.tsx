f0eaa76efc8a1c62e020a2fde4b329bedae0e4b4
tailselrayo <rodtailselrayo@gmail.com>
2023-08-30 15:30:47 +0200

    feat(ui): added tooltip component

---
+++ src/components/ui/Tooltip.tsx
@@ -0,0 +1,31 @@
+import { motion } from "framer-motion";
+import { ReactNode, useState } from "react";
+
+interface TooltipProps {
+  label: string;
+  disabled?: boolean;
+  children: ReactNode;
+}
+
+export function Tooltip(props: TooltipProps) {
+  const [isMouseOver, setIsMouseOver] = useState(false);
+  
+  const isTooltipOn = !props.disabled&&isMouseOver
+
+  return (
+    <div
+      onMouseEnter={() => setIsMouseOver(true)}
+      onMouseLeave={() => setIsMouseOver(false)}
+      className="relative"
+    >
+      <motion.div
+        initial={{ scale: 1, x: "-50%", y: "-50%", opacity: 0}}
+        animate={{  scale: isTooltipOn ? 1 : 0, y: isTooltipOn ? "-100%": "-50%", opacity: isTooltipOn ? 1:0 }}
+        className="p-2 bg-black text-white absolute left-1/2 z-20 w-max rounded-md drop-shadow-md -top-2"
+      >
+        <span >{props.label}</span>
+      </motion.div>
+      {props.children}
+    </div>
+  );
+}
